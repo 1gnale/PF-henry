fa6a99dbaa9f29b1c847b315033fd2e6195a175c
lucasibar <lucas.ibar.soy.henry@gmail.com>
2023-02-01 12:49:11 -0300

    destructurinNameParams

--- api/src/controllers/getByName.js
+++ api/src/controllers/getByName.js
@@ -1,16 +1,23 @@
 const { Products } = require("../../db");
 
-module.exports = {
-    getByName: async function (name) {
-        try{
+const getByName = async (req, res) => {
+    const name = req.match.params.name
+        if(name){
+            try{
             const productsBDD =  await Products.findAll({
                 where:{name: name}
             })
             return res.status(200).json(productsBDD)
-        }
-        catch { 
+            }
+            catch { 
             return res.status(400).json('Hubo un error en la coneccion de la base de datos')
+            } 
+        }else{
+            return res.status(200).json('No se enviaron datos')
         }
-    } 
-}  
-       
+}
+
+module.exports={
+    getByName
+}
+        
