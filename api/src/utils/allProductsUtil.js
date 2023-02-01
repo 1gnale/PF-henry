b20ea2b1939766df9cb640e6aa3335ff71960a03
Ignacio <nosoyignacio@gmail.com>
2023-02-01 11:18:11 -0300

    forced commit

---
+++ api/src/utils/allProductsUtil.js
@@ -0,0 +1,34 @@
+const { Product, Category } = require("../db")
+
+const allProducts = async () => {
+    const dbProducts = await Product.findAll({
+        include:{
+            model: Category,
+            attributes: ["name"],
+            through: {
+                attributes:[]
+            }
+        }
+    })
+
+    const dbProductsClean = dbProducts.map((e) => {
+        return{
+            id: e.id,
+            name: e.name,
+            price: e.price,
+            stock: e.stock,
+            description: e.description,
+            imageURL: e.imageURL,
+            weight: e.weight,
+            height: e.height,
+            offer: e.offer
+        }
+    })
+
+    return dbProductsClean
+}
+
+
+module.exports = {
+    allProducts
+}
