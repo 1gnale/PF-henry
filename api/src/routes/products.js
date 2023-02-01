133eee705eef082b63e2c7ce10a9722ddf64b6a8
fabriziocl <f.castrolopez@hotmail.com>
2023-02-01 12:11:24 -0300

    changed folder name

---
+++ api/src/routes/products.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { allProducts } = require('../utils/allProductsUtil');
+const router = Router();
+const getByName = require('../controllers/getByName')
+
+products.get('/', )
+products.get('/:id', )
+products.post('/', )
+
+module.exports = router;
