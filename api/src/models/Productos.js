0346e0c6b879c087a89c016cf8629314752cd2d8
lucasibar <lucas.ibar.soy.henry@gmail.com>
2023-02-01 10:27:57 -0300

    Modelo Producto y Categoria

---
+++ api/src/models/Productos.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require('sequelize');
+
+
+module.exports = (sequelize) => {
+
+  sequelize.define('Productos', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING
+    },
+    img: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://i.pinimg.com/originals/37/2c/db/372cdbb7af08ad27524ef53c53e1ba8d.jpg',
+    }, 
+    descripcion:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },    
+    precio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stockActual: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    oferta: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+  });
+};
