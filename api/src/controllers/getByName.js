9b3dd2badf5cd897f58ff3475a32c993c5325568
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-02 13:32:12 -0300

    fixed getByName route

--- api/src/controllers/getByName.js
+++ api/src/controllers/getByName.js
@@ -1,13 +1,13 @@
 const { Products } = require("../db.js");
 
 const getByName = async (req, res, next) => {
-    const name = req.params
+    const {name} = req.query
         if(name){
             try{
-            const productsBDD =  await Products.findAll({
-                where:{name: name}
-            })
-            return res.status(200).json(productsBDD)
+            const productsBDD =  await Products.findAll() 
+            const results = productsBDD.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
+            if (results.length > 0) return res.status(200).json(results)
+            else return res.status(404).json("No hay productos con ese nombre")
             }
             catch { 
             return res.status(400).json('Hubo un error en la coneccion de la base de datos')
