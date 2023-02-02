250e18c71b5b336da72a8b5d10355ed6edb9566f
fabriziocl <f.castrolopez@hotmail.com>
2023-02-02 12:22:57 -0300

    added specific messages for errors

--- api/src/middlewares/validatePostCategory.js
+++ api/src/middlewares/validatePostCategory.js
@@ -1,6 +1,6 @@
 const validatePostCategory = (req, res, next) => {
     const { name } = req.body;
-    if (!name) return res.status(400).send(`Missing data`);
+    if (!name) return res.status(400).send(`${name} value is required`);
     next()
     }
 module.exports={
