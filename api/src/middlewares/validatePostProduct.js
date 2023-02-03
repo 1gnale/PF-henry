588f25a51af819fdfa83d14904ff336ef2eee866
Ignacio <nosoyignacio@gmail.com>
2023-02-03 11:42:34 -0300

    fix values validate

--- api/src/middlewares/validatePostProduct.js
+++ api/src/middlewares/validatePostProduct.js
@@ -2,15 +2,15 @@ const validatePostProduct = (req, res, next) => {
     const {
         name, height, weight, img, description, price, stock, offert, category
     } = req.body;
-    if (!name) return res.status(400).send(`${name} value is required`);
-    if (!height) return res.status(400).send(`${height} value is required`);
-    if (!weight) return res.status(400).send(`${weight} value is required`);
-    if (!img) return res.status(400).send(`${img} value is required`);
-    if (!description) return res.status(400).send(`${description} value is required`);
-    if (!price) return res.status(400).send(`${price} value is required`);
-    if (!stock) return res.status(400).send(`${stock} value is required`);
-    if (!offert) return res.status(400).send(`${offert} value is required`);
-    if (!category) return res.status(400).send(`${category} value is required`)
+    if (!name) return res.status(400).send(`Name value is required`);
+    if (!height) return res.status(400).send(`Height value is required`);
+    if (!weight) return res.status(400).send(`Weight value is required`);
+    if (!img) return res.status(400).send(`Img value is required`);
+    if (!description) return res.status(400).send(`Description value is required`);
+    if (!price) return res.status(400).send(`Price value is required`);
+    if (!stock) return res.status(400).send(`Stock value is required`);
+    if (!offert) return res.status(400).send(`Offert value is required`);
+    if (!category) return res.status(400).send(`Category value is required`)
     next()
 }
 
