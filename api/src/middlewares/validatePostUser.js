14caaae4992d2190e2b6a3d135e04c09d9f92685
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2023-02-07 17:57:19 -0300

    Update validatePostUser.js

--- api/src/middlewares/validatePostUser.js
+++ api/src/middlewares/validatePostUser.js
@@ -7,11 +7,11 @@ const validatePostUser = (req, res, next) => {
         picture,
         birthday
     } = req.body
-    if(!email) return res.status(404).send(`${email} field is required`)
-    if(!fullName) return res.status(404).send(`${fullName} field is required`)
-    if(!username) return res.status(404).send(`${user_id} field is required`)
-    if(!picture) return res.status(404).send(`${picture} field is required`)
-    if(!birthday) return res.status(404).send(`${birthday} field is required`)
+    if(!email) return res.status(404).send(`email field is required`)
+    if(!fullName) return res.status(404).send(`fullName field is required`)
+    if(!username) return res.status(404).send(`user_id field is required`)
+    if(!picture) return res.status(404).send(`picture field is required`)
+    if(!birthday) return res.status(404).send(`birthday field is required`)
     next()
 }
 
