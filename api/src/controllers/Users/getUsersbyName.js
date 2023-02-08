127ec46b690bb3075401f603ff703577d7cd904a
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-08 11:00:49 -0300

    new directories in controllers + user controllers

---
+++ api/src/controllers/Users/getUsersbyName.js
@@ -0,0 +1,23 @@
+const { User } = require("../../db")
+
+const getByName = async (req, res, next) => {
+    const { fullName } = req.query
+        if(fullName){
+            try{
+            const usersDB =  await User.findAll() 
+            console.log(usersDB)
+            const results = usersDB.filter(e => e.fullName.toLowerCase().includes(fullName.toLowerCase()))
+            if (results.length > 0) return res.status(200).json(results)
+            else return res.status(404).json("No hay productos con ese nombre")
+            }
+            catch { 
+            return res.status(400).json('Hubo un error en la coneccion de la base de datos')
+            } 
+        }else{
+            next()
+        }
+}
+
+module.exports = {
+    getByName
+}
