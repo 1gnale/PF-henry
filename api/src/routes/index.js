44e6fa5691ff6461310b1ab5317c3f9df9ec55b9
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-08 11:05:49 -0300

    added user router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const categories = require('./categories');
 const products = require('./products');
+const users = require('./users')
 
 router.use('/categories', categories);
 router.use('/products', products);
+router.use('/users', users)
 
 module.exports = router;
