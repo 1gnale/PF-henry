d9add3fec95e48533055adaff28c98610981fa13
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-09 16:47:00 -0300

    removed Favorite model and changed table references in favorite/routes

--- api/src/controllers/Favorites/postFavorite.js
+++ api/src/controllers/Favorites/postFavorite.js
@@ -1,21 +1,19 @@
-const { User, Favorite, Product } = require("../../db");
+const { UserFavorites } = require("../../db");
 
 const postFavorite = async(req, res) => {
    try { 
     const { userId, productId } = req.body
 
-    const dbFavorite = await Favorite.findOrCreate({
+    const dbFavorite = await UserFavorites.findOrCreate({
             where: {
                 userId: userId,
                 productId: productId
             }
         })
-
     res.status(200).json("Favorite added to DB")
     } catch(error) {
         res.status(403).json({msg: error})
     }
-    
 };
 
 module.exports= {
