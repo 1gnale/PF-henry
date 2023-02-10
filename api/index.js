0ec64fec2debbec5775970c95a2f9ec51eb1e9c5
1gnale <110366110+1gnale@users.noreply.github.com>
2023-02-10 21:31:59 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config()
-const {SV_PORT} = process.env
+const {PORT} = process.env
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(SV_PORT, () => {
-    console.log(`%s listening at ${SV_PORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
