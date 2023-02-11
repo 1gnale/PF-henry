97c9d8ce97f5240d3adbd3ba1391410684a7dede
tedelcopp <tomas.edelcopp@gmail.com>
2023-02-11 14:46:50 -0300

    LocalStorage

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-require("dotenv").config()
-const {PORT} = process.env
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+const { PORT } = process.env;
 
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
--- api/src/controllers/Products/getAllProducts.js
+++ api/src/controllers/Products/getAllProducts.js
@@ -1,14 +1,15 @@
-const { allProducts } = require("../../utils/allProductsUtil")
+const { allProducts } = require("../../utils/allProductsUtil");
 
 const getAllProduct = async (req, res) => {
-    try {
-        let results = await allProducts()
-        res.status(200).json(results)
-    } catch (error) {
-        res.status(400).json({ error: "db vacia" })
-    }
-}
+  try {
+    let results = await allProducts();
 
-module.exports={
-    getAllProduct
-}
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(400).json({ error: "db vacia" });
+  }
+};
+
+module.exports = {
+  getAllProduct,
+};
