fd13c100a2aca3f0cc3e3804c12a880d5f225196
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2023-02-17 04:53:29 -0300

    added additional midware to protect routes

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -5,12 +5,13 @@ const { getProductById } = require('../controllers/Products/getProductById');
 const { validatePostProduct } = require ("../middlewares/validatePostProduct");
 const { postProduct } = require("../controllers/Products/postProduct");
 const { updateProduct } = require("../controllers/Products/putProducts")
+const { verifyToken } = require('../middlewares/verifyToken')
 const productsRouter = Router();
 
 
 productsRouter.get('/', getByName, getAllProduct)
 productsRouter.get('/:id', getProductById)
-productsRouter.post('/', validatePostProduct, postProduct)
+productsRouter.post('/', verifyToken, validatePostProduct, postProduct)
 productsRouter.put('/', updateProduct)
 
 module.exports = productsRouter;
