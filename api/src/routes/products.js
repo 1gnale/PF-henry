cb2587264f480ca651c0d274b2cc62892233f398
1gnale <110366110+1gnale@users.noreply.github.com>
2023-02-18 08:50:09 +0100

    Update products.js

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -11,7 +11,7 @@ const productsRouter = Router();
 
 productsRouter.get('/', getByName, getAllProduct)
 productsRouter.get('/:id', getProductById)
-productsRouter.post('/', verifyToken, validatePostProduct, postProduct)
+productsRouter.post('/', validatePostProduct, postProduct)
 productsRouter.put('/', updateProduct)
 
 module.exports = productsRouter;
