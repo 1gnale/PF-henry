2ca8045ff54c38e5209396e1188dbf5d5929d246
GiuGobbi <giuliagobbi1995@gmail.com>
2023-02-19 18:09:50 -0300

    change stock automatically when posting order

--- api/src/controllers/Order/postOrder.js
+++ api/src/controllers/Order/postOrder.js
@@ -1,12 +1,13 @@
-const { OrderItems, OrderDetails } = require("../../db")
+const { OrderItems, OrderDetails, User, Product } = require("../../db")
+const {findProductStock} = require("../../utils/updateStockUtil")
 
 const postOrder = async(req, res) => {
     try {
-        const { userId, totalAmount, status, paypalId, products } = req.body;
-
+        const { email, totalAmount, status, paypalId, products } = req.body;
+        const user = await User.findOne({where: {email: email}})
 
         const newOrder = await OrderDetails.create({
-            userId: userId,
+            userId: user.dataValues.id,
             totalAmount: totalAmount,
             payPalOrderId: paypalId,
             status: status.toLowerCase()
@@ -14,13 +15,20 @@ const postOrder = async(req, res) => {
 
         const orderIdDB = newOrder.dataValues.id
 
-        const soldItems = await Promise.all(products.map(i => 
-            OrderItems.create({
+        const soldItems = await Promise.all(products.map(async i => 
+            await OrderItems.create({
                orderId: orderIdDB,
                productId: i.productId,
                quantity: i.quantity
             })))
 
+        const updateProduct = await Promise.all(products.map( async p => {
+            oldStock = await findProductStock(p.productId)
+            await Product.update({
+                stock: oldStock - p.quantity
+            }, {where: {id: p.productId}})
+            }
+        ))
         return res.status(201).json("New product created correctly")
     }
     catch (error){
