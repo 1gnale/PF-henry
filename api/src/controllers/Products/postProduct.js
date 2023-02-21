18270f493886dda341f1f933ab6e748abf8ef63b
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2023-02-21 16:19:01 -0300

    set config to send files to cloudinary

--- api/src/controllers/Products/postProduct.js
+++ api/src/controllers/Products/postProduct.js
@@ -1,45 +1,51 @@
 const { Product, Category } = require("../../db");
 const { cloudinary } = require("../../cloudinary");
+const fs = require("fs");
 
 const postProduct = async (req, res) => {
-    
-    try {
-        const { name, height, weight, img, description, price, stock, offert, category } = req.body;
+  try {
+    const { name, height, weight, description, price, stock, offert, category } = req.body;
+    const file = req.file;
 
-        const uploadedRes = await cloudinary.uploader.upload(img, {public_id: name})
+    const uploadedRes = await cloudinary.uploader.upload(fs.readFileSync(file.path), {
+      public_id: name
+    });
 
-        const modCategories = await category.map(c => {
-            const lowercaseCategory = c.toLowerCase();
-            const capitalizedCategory = lowercaseCategory.charAt(0).toUpperCase() + lowercaseCategory.slice(1);
-            return capitalizedCategory;
-          });
+    const modCategories = await category.map(c => {
+      const lowercaseCategory = c.toLowerCase();
+      const capitalizedCategory = lowercaseCategory.charAt(0).toUpperCase() + lowercaseCategory.slice(1);
+      return capitalizedCategory;
+    });
 
-        const newProduct = await Product.create({
-            name: name,
-            height: height,
-            weight: weight,
-            img: uploadedRes.url,
-            description: description,
-            price: price,
-            stock: stock,
-            offert: offert
+    const newProduct = await Product.create({
+      name: name,
+      height: height,
+      weight: weight,
+      img: uploadedRes.url,
+      description: description,
+      price: price,
+      stock: stock,
+      offert: offert
+    });
+
+    const dbCategories = await Promise.all(
+      modCategories?.map(c =>
+        Category.findOrCreate({
+          where: {
+            name: c
+          }
         })
-        const dbCategories = await Promise.all(modCategories?.map(c => 
-            Category.findOrCreate({
-                where: {
-                    name: c
-                }
-            })))
-        const tempObj = dbCategories.map(([temp, created]) => temp)
-        await newProduct.addCategory(tempObj)
-        return res.status(201).json("New product created successfully")
-    }
-    catch (error){
-        return res.status(400).json({error: error.message})
-    }
-}
+      )
+    );
+
+    const tempObj = dbCategories.map(([temp, created]) => temp)
+    await newProduct.addCategory(tempObj)
+    return res.status(201).json("New product created successfully")
+  } catch (error) {
+    return res.status(400).json({ error: error.message })
+  }
+};
 
 module.exports = {
-    postProduct
-}
-
+  postProduct
+};
